c06f85284e67a5511d8ba1e73243ba0461583dc6
Ozayer-Coder <ozayer212714@gmail.com>
2026-07-06 15:43:36 +0600

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,35 +1,50 @@
-// navbar scroll effect 
+// navbar scroll effect
 const navbar = document.getElementById("navbar");
 
 window.addEventListener("scroll", function () {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+// animated text
+new Typed("#typing", {
+  strings: [
+    "Aspiring Full Stack Developer",
+    "Frontend Developer",
+    "Problem Solver",
+    "Lifelong Learner",
+  ],
+  typeSpeed: 60,
+  backSpeed: 40,
+  backDelay: 1500,
+  loop: true,
+});
+// live clock
+function updateClock() {
+  const now = new Date();
+  // Format to 12-hour or 24-hour time
+  document.getElementById("live-clock").textContent = now.toLocaleTimeString();
+}
+// Update the clock every 1000 milliseconds (1 second)
+setInterval(updateClock, 1000);
+// Initialize the clock immediately on page load
+updateClock();
 
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
+// form validation
+let form = document.getElementById("contactForm");
+form.addEventListener(
+  "submit",
+  (event) => {
+    if (!form.checkValidity()) {
+      event.preventDefault();
+      event.stopPropagation();
     } else {
-        navbar.classList.remove("scrolled");
+      alert("Thank you! Your message has been sent.");
     }
 
-});
-// animated text 
-new Typed("#typing", {
-    strings: [
-        "Aspiring Full Stack Developer",
-        "Frontend Developer",
-        "Problem Solver",
-        "Lifelong Learner"
-    ],
-    typeSpeed: 60,
-    backSpeed: 40,
-    backDelay: 1500,
-    loop: true
-});
-// live clock 
- function updateClock() {
-            const now = new Date();
-            // Format to 12-hour or 24-hour time
-            document.getElementById('live-clock').textContent = now.toLocaleTimeString();
-        }
-        // Update the clock every 1000 milliseconds (1 second)
-        setInterval(updateClock, 1000);
-        // Initialize the clock immediately on page load
-        updateClock();
+    form.classList.add("was-validated");
+  },
+  false,
+);
